301137f9f85e65881dd6ff314a99b5681b6815d1
Andrew Vincent <92338473+aavin95@users.noreply.github.com>
2022-10-28 08:45:42 -0400

    Time: 0 ms (100.00%), Space: 6 MB (38.20%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -8,13 +8,15 @@ public:
         if(n == 1){
             return 1;
         }
-        vector<int> fib_list;    
-        fib_list.resize(n+1);
-        fib_list[0] = 0;
-        fib_list[1] = 1;
+        int f1 = 0;
+        int f2 = 1;
+        int result = 0;
         for(int i = 2; i <= n; i++){
-            fib_list[i] = fib_list[i-1]+fib_list[i-2];
+            int curr = (f1 + f2);
+            f1 = f2;
+            f2 = curr;
+            result = curr; 
         }
-        return fib_list[n];
+        return result;
     }
 };
