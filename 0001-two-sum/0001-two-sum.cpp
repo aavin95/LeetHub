fe599f48ffd44319a6316d4d17a93adc64442cbe
Andrew Vincent <92338473+aavin95@users.noreply.github.com>
2022-12-16 09:05:24 -0500

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans = {0,1};
-        if(nums.size() == 2){
-            return ans;
-        }
-        //num, index
-        unordered_map<int, int> map;
-        
+        unordered_map<int, vector<int>> map;
+        vector<int> n;
         for(int i = 0; i < nums.size(); i++){
-            map[nums[i]] = i;
-        }
-        for(int i = 0; i < nums.size(); i++){
-            int temp = target - nums[i];
-            if(map.find(temp) != map.end() && map[temp] != i){
-                ans[0] = i;
-                ans[1] = map[temp];
+            map[nums[i]].push_back(i);
+            if(target - nums[i] == nums[i]){
+                if(map[nums[i]].size() == 2){
+                    n.push_back(map[nums[i]][0]);
+                    n.push_back(map[nums[i]][1]);
+                    return n;
+                }
+            }
+            else if(map.find(target - nums[i]) != map.end() ){
+                int hold = map[target - nums[i]][0];
+                n.push_back(hold);
+                n.push_back(i);
+                return n;
             }
         }
-        return ans;
+        return n;
     }
 };
